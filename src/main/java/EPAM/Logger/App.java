325ca26cfcb657c1e1d06a22f07599886daf10d6
prabhatkadam18 <prabhatkadam18@gmail.com>
2020-07-01 19:16:42 +0530

    first commit

---
+++ src/main/java/EPAM/Logger/App.java
@@ -0,0 +1,16 @@
+package EPAM.Logger;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class App
+{
+    private static final Logger Logger = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        Logger.debug("This is Debug");
+        Logger.info("This is Info");
+        Logger.warn("This is Warn");
+        Logger.error("This is Error");
+        Logger.fatal("This is Fatal");
+    }
+}
